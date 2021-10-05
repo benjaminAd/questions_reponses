276158c3147c70fea6c811c93a309e8d46a73342
Benjamin ADOLPHE <adolphe906@gmail.com>
2021-10-05 16:32:45 +0200

    Appel du block provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
+import 'package:questions_reponses/cubit/question_cubit.dart';
 import 'package:questions_reponses/home.dart';
 import 'package:questions_reponses/provider/question_provider.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: (context) => QuestionProvider(),
-    child: MyApp(),
-  ));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -18,18 +17,12 @@ class MyApp extends StatelessWidget {
       title: 'Questions réponses',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blueGrey,
       ),
-      home: HomePage(),
+      home: BlocProvider(
+        create: (_) => QuestionCubit(),
+        child: HomePage(),
+      ),
     );
   }
 }
